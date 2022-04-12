1a58498b3e492163c514e6e98b160b708f6fb128
SiddharthKhattar <skhattar111@gmail.com>
2022-04-13 02:50:22 +0530

    Add Modal Button

--- index.php
+++ index.php
@@ -114,6 +114,7 @@ Edit Modal
 </div>
 
 
+
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <a class="navbar-brand" href="#">PHP CRUD</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
