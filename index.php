3d16c326c43eefb6c66de57be2fa7924c7273b7f
SiddharthKhattar <skhattar111@gmail.com>
2022-04-13 11:53:03 +0530

    Changes Example Modal to Edit Modal

--- index.php
+++ index.php
@@ -65,16 +65,17 @@ if ($_SERVER['REQUEST_METHOD'] == "POST"){
 
 
   <body>
-  <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalLong">
-    Launch demo modal
+
+  <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#editModal">
+    Edit Modal
   </button>
     
     <!-- Modal -->
-    <div class="modal fade" id="exampleModalLong" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
+    <div class="modal fade" id="editModal" tabindex="-1" role="dialog" aria-labelledby="editModalLabel" aria-hidden="true">
         <div class="modal-dialog" role="document">
           <div class="modal-content">
               <div class="modal-header">
-              <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
+              <h5 class="modal-title" id="editModalLabel">Modal title</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
@@ -152,7 +153,7 @@ if ($_SERVER['REQUEST_METHOD'] == "POST"){
               <th scope="col">S. No.</th>
               <th scope="col">Note Title </th>
               <th scope="col">Note Description</th>
-              <th scope="col">Handle</th>
+              <th scope="col">Action</th>
             </tr>
           </thead>
           <tbody>
